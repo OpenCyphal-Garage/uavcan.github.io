3b471e58e03df17a3a4166315b859d5340b46289
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-08-29 18:19:25 +0300

    Example node updates

--- Examples/simple_sensor_node.c
+++ Examples/simple_sensor_node.c
@@ -1,11 +1,14 @@
 /*
  * This program is an example implementation of an extremely lightweight UAVCAN node with zero third-party dependencies.
  * This example is based on Linux SocketCAN, but can be easily adapted to any other platform.
- * Compiler invocation command:
- *     gcc simple_sensor_node.c -lrt -std=gnu99
+ *
+ * GCC invocation command:
+ *     gcc simple_sensor_node.c -lrt -std=gnu99 -o simple_sensor_node
+ * With warnings:
+ *     gcc simple_sensor_node.c -lrt -std=gnu99 -o simple_sensor_node -Wall -Werror -Wextra -pedantic -Wsign-conversion
  *
  * Author: Pavel Kirienko <pavel.kirienko@zubax.com>
- * License: Public domain
+ * License: CC0, no copyright reserved
  * Language: C99
  */
 
@@ -89,7 +92,7 @@ uint64_t get_monotonic_usec(void)
     {
         return 0;
     }
-    return (uint64_t)ts.tv_sec * 1000000ULL + ts.tv_nsec / 1000UL;
+    return (uint64_t)ts.tv_sec * 1000000ULL + (uint64_t)ts.tv_nsec / 1000UL;
 }
 /*
  * End of the platform-specific part
@@ -98,24 +101,40 @@ uint64_t get_monotonic_usec(void)
 /*
  * UAVCAN transport layer
  */
+/// Arbitrary priority values
+static const uint8_t PRIORITY_HIGHEST = 0;
+static const uint8_t PRIORITY_HIGH    = 8;
+static const uint8_t PRIORITY_MEDIUM  = 16;
+static const uint8_t PRIORITY_LOW     = 24;
+static const uint8_t PRIORITY_LOWEST  = 31;
+
 static uint8_t uavcan_node_id;
 
-int uavcan_broadcast(uint16_t data_type_id, uint8_t transfer_id, const uint8_t* payload, uint16_t payload_len)
+int uavcan_broadcast(uint8_t priority,
+                     uint16_t data_type_id,
+                     uint8_t transfer_id,
+                     const uint8_t* payload,
+                     uint16_t payload_len)
 {
-    if (payload == NULL || payload_len > 8)
+    if (payload == NULL || payload_len > 7)
     {
         return -1;  // In this super-simple implementation we don't support multi-frame transfers.
     }
 
-    const uint32_t can_id =
-        (transfer_id & 7) |              // Transfer ID
-        (1 << 3) |                       // Last Frame (always set)
-        (0 << 4) |                       // Frame Index (always 0)
-        ((uavcan_node_id & 127) << 10) | // Source Node ID
-        (2 << 17) |                      // Transfer Type (always 2 - Message Broadcast)
-        ((data_type_id & 1023) << 19);   // Data Type ID
+    if (priority > 31)
+    {
+        return -1;
+    }
 
-    return can_send(can_id, payload, payload_len);
+    const uint32_t can_id = ((uint32_t)priority << 24) | ((uint32_t)data_type_id << 8) | (uint32_t)uavcan_node_id;
+
+    uint8_t payload_with_tail_byte[8];
+
+    memcpy(payload_with_tail_byte, payload, payload_len);
+
+    payload_with_tail_byte[payload_len] = 0xC0 | (transfer_id & 31);
+
+    return can_send(can_id, payload_with_tail_byte, payload_len + 1);
 }
 
 /*
@@ -123,56 +142,71 @@ int uavcan_broadcast(uint16_t data_type_id, uint8_t transfer_id, const uint8_t* 
  */
 uint16_t make_float16(float value)
 {
-    uint16_t hbits = signbit(value) << 15;
-    if (value == 0.0F)
+    union fp32
     {
-        return hbits;
-    }
-    if (isnan(value))
+        uint32_t u;
+        float f;
+    };
+
+    const union fp32 f32infty = { 255U << 23 };
+    const union fp32 f16infty = { 31U << 23 };
+    const union fp32 magic = { 15U << 23 };
+    const uint32_t sign_mask = 0x80000000U;
+    const uint32_t round_mask = ~0xFFFU;
+
+    union fp32 in;
+    uint16_t out = 0;
+
+    in.f = value;
+
+    uint32_t sign = in.u & sign_mask;
+    in.u ^= sign;
+
+    if (in.u >= f32infty.u)
     {
-        return hbits | 0x7FFFU;
-    }
-    if (isinf(value))
-    {
-        return hbits | 0x7C00U;
-    }
-    int exp;
-    (void)frexp(value, &exp);
-    if (exp > 16)
-    {
-        return hbits | 0x7C00U;
-    }
-    if (exp < -13)
-    {
-        value = ldexp(value, 24);
+        out = (in.u > f32infty.u) ? 0x7FFFU : 0x7C00U;
     }
     else
     {
-        value = ldexp(value, 11 - exp);
-        hbits |= ((exp + 14) << 10);
+        in.u &= round_mask;
+        in.f *= magic.f;
+        in.u -= round_mask;
+        if (in.u > f16infty.u)
+        {
+            in.u = f16infty.u;
+        }
+        out = (uint16_t)(in.u >> 13);
     }
-    const int32_t ival = (int32_t)value;
-    hbits |= (uint16_t)(((ival < 0) ? (-ival) : ival) & 0x3FFU);
-    float diff = fabs(value - (float)ival);
-    hbits += diff >= 0.5F;
-    return hbits;
+
+    out |= (uint16_t)(sign >> 16);
+
+    return out;
 }
 
 /*
  * Application logic
  */
- // These constants are defined for the standard data type uavcan.protocol.NodeStatus
-enum node_status
+/// Defined for the standard data type uavcan.protocol.NodeStatus
+enum node_health
 {
-    STATUS_OK           = 0,
-    STATUS_INITIALIZING = 1,
-    STATUS_WARNING      = 2,
-    STATUS_CRITICAL     = 3,
-    STATUS_OFFLINE      = 15
+    HEALTH_OK       = 0,
+    HEALTH_WARNING  = 1,
+    HEALTH_ERROR    = 2,
+    HEALTH_CRITICAL = 3
+};
+
+/// Defined for the standard data type uavcan.protocol.NodeStatus
+enum node_mode
+{
+    MODE_OPERATIONAL     = 0,
+    MODE_INITIALIZATION  = 1,
+    MODE_MAINTENANCE     = 2,
+    MODE_SOFTWARE_UPDATE = 3,
+    MODE_OFFLINE         = 7
 };
 
 /// Standard data type: uavcan.protocol.NodeStatus
-int publish_node_status(enum node_status node_status, uint16_t vendor_specific_status_code)
+int publish_node_status(enum node_health health, enum node_mode mode, uint16_t vendor_specific_status_code)
 {
     static uint64_t startup_timestamp_usec;
     if (startup_timestamp_usec == 0)
@@ -180,26 +214,26 @@ int publish_node_status(enum node_status node_status, uint16_t vendor_specific_s
         startup_timestamp_usec = get_monotonic_usec();
     }
 
-    uint8_t payload[6];
+    uint8_t payload[7];
 
-    // Uptime in seconds - first 28 bits of the message
+    // Uptime in seconds
     const uint32_t uptime_sec = (get_monotonic_usec() - startup_timestamp_usec) / 1000000ULL;
     payload[0] = (uptime_sec >> 0)  & 0xFF;
     payload[1] = (uptime_sec >> 8)  & 0xFF;
     payload[2] = (uptime_sec >> 16) & 0xFF;
-    payload[3] = ((uptime_sec >> 24) & 0x0F) << 4;
+    payload[3] = (uptime_sec >> 24) & 0xFF;
 
-    // Status - next 4 bits
-    payload[3] |= (uint8_t)node_status & 0x0F;
+    // Health and mode
+    payload[4] = ((uint8_t)health << 6) | ((uint8_t)mode << 3);
 
     // Vendor-specific status code
-    payload[4] = (vendor_specific_status_code >> 0) & 0xFF;
-    payload[5] = (vendor_specific_status_code >> 8) & 0xFF;
+    payload[5] = (vendor_specific_status_code >> 0) & 0xFF;
+    payload[6] = (vendor_specific_status_code >> 8) & 0xFF;
 
-    static const uint16_t data_type_id = 550;
+    static const uint16_t data_type_id = 341;
     static uint8_t transfer_id;
-    transfer_id += 1;
-    return uavcan_broadcast(data_type_id, transfer_id, payload, sizeof(payload));
+
+    return uavcan_broadcast(PRIORITY_LOW, data_type_id, transfer_id++, payload, sizeof(payload));
 }
 
 /// Standard data type: uavcan.equipment.air_data.TrueAirspeed
@@ -214,10 +248,10 @@ int publish_true_airspeed(float mean, float variance)
     payload[2] = (f16_variance >> 0) & 0xFF;
     payload[3] = (f16_variance >> 8) & 0xFF;
 
-    static const uint16_t data_type_id = 290;
+    static const uint16_t data_type_id = 1020;
     static uint8_t transfer_id;
     transfer_id += 1;
-    return uavcan_broadcast(data_type_id, transfer_id, payload, sizeof(payload));
+    return uavcan_broadcast(PRIORITY_MEDIUM, data_type_id, transfer_id, payload, sizeof(payload));
 }
 
 int compute_true_airspeed(float* out_airspeed, float* out_variance)
@@ -254,7 +288,7 @@ int main(int argc, char** argv)
     /*
      * Main loop
      */
-    enum node_status node_status = STATUS_INITIALIZING;
+    enum node_health health = HEALTH_OK;
 
     for (;;)
     {
@@ -265,16 +299,16 @@ int main(int argc, char** argv)
         if (airspeed_computation_result == 0)
         {
             const int publication_result = publish_true_airspeed(airspeed, airspeed_variance);
-            node_status = (publication_result < 0) ? STATUS_WARNING : STATUS_OK;
+            health = (publication_result < 0) ? HEALTH_ERROR : HEALTH_OK;
         }
         else
         {
-            node_status = STATUS_WARNING;
+            health = HEALTH_ERROR;
         }
 
         const uint16_t vendor_specific_status_code = rand(); // Can be used to report vendor-specific status info
 
-        (void)publish_node_status(node_status, vendor_specific_status_code);
+        (void)publish_node_status(health, MODE_OPERATIONAL, vendor_specific_status_code);
 
         usleep(500000);
     }
