b433973013894b3dbf266abc117ba719e4f269db
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-09-06 16:38:56 +0300

    Tutorial build scripts

---
+++ Implementations/Libuavcan/Tutorials/rebuild_all_tutorials_using_cmake.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ">>> Purging build directories..."
+rm -rf */build &> /dev/null
+
+set -e
+
+for cmake_script in */CMakeLists.txt ; do
+    d=`dirname $cmake_script`
+
+    echo ">>> Creating $d/build..."
+    mkdir $d/build
+    cd $d/build
+
+    echo ">>> Running CMake..."
+    cmake ..
+
+    echo ">>> Running make..."
+    make -j8
+
+    echo ">>> Tutorial '$d' built successfully"
+    cd ../..
+done
