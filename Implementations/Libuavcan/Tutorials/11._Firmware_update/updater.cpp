23f47be591bfae9cd5d0295c6069271bd6c0cbf3
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-09-28 10:38:56 +0300

    Libuavcan tut 11 - finished the updater

--- Implementations/Libuavcan/Tutorials/11._Firmware_update/updater.cpp
+++ Implementations/Libuavcan/Tutorials/11._Firmware_update/updater.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstdlib>
+#include <cctype>
+#include <vector>
+#include <algorithm>
 #include <unistd.h>
 #include <uavcan/uavcan.hpp>
 
@@ -58,17 +61,13 @@ class ExampleFirmwareVersionChecker final : public uavcan::IFirmwareVersionCheck
                                      FirmwareFilePath& out_firmware_file_path)
     override
     {
-        (void)node_id;
-        (void)node_info;
-        (void)out_firmware_file_path;
-
         /*
          * We need to make the decision, given the inputs, whether the node requires an update.
          * This part of the logic is deeply application-specific, so the solution provided here may not work
-         * for real-world applications.
+         * for some real-world applications.
          *
          * It is recommended to refer to the PX4 autopilot project or to the APM project for a real-world
-         * use case.
+         * example.
          *
          * Both PX4 and APM leverage a class provided by the libuavcan's POSIX platform driver -
          * uavcan_posix::FirmwareVersionChecker - which implements a generic firmware version checking algorithm.
@@ -76,32 +75,89 @@ class ExampleFirmwareVersionChecker final : public uavcan::IFirmwareVersionCheck
          *   1. Check if the file system has a firmware file for the given node.
          *      If not, exit - update will not be possible anyway.
          *   2. Compare the CRC of the local firmware image for the given node with CRC of the firmware the node is
-         *      running at the moment (the latter is available via the node info argument).
+         *      running at the moment (the latter is available via the node info argument in this method).
          *   3. Request an update if CRC don't match, otherwise do not request an update.
          *
          * In this example, we're using a simpler logic.
+         *
+         * Firmware file name pattern used in the example is as follows:
+         *      <node-name>-<hw-major>.<hw-minor>-<sw-major>.<sw-minor>.<vcs-hash-hex>.uavcan.bin
          */
+
+        std::cout << "Checking firmware version of node " << int(node_id.get()) << "; node info:\n"
+                  << node_info << std::endl;
 
         /*
          * Looking for matching firmware files.
          */
-        const auto files = findAvailableFirmwareFiles(node_info);
-        std::cout << "Matching firmware files for node '" << node_info.name.c_str() << "':" << std::endl;
-        for (auto x: glob_matches)
-        {
-            std::cout << "\t" << x << std::endl;
-        }
-
-        /*
-         * If no matching files were found, we obviously can't perform the update.
-         */
-        if (glob_matches.empty())
+        auto files = findAvailableFirmwareFiles(node_info);
+        if (files.empty())
         {
             std::cout << "No firmware files found for this node" << std::endl;
             return false;
         }
 
-        return false;   // TODO
+        std::cout << "Matching firmware files:";
+        for (auto x: files)
+        {
+            std::cout << "\n\t" << x << "\n" << parseFirmwareFileName(x.c_str()) << std::endl;
+        }
+
+        /*
+         * Looking for the firmware file with highest version number
+         */
+        std::string best_file_name;
+        unsigned best_combined_version = 0;
+        for (auto x: files)
+        {
+            const auto inf = parseFirmwareFileName(x.c_str());
+            const unsigned combined_version = (unsigned(inf.software_version.major) << 8) + inf.software_version.minor;
+            if (combined_version >= best_combined_version)
+            {
+                best_combined_version = combined_version;
+                best_file_name = x;
+            }
+        }
+
+        std::cout << "Preferred firmware: " << best_file_name << std::endl;
+
+        const auto best_firmware_info = parseFirmwareFileName(best_file_name.c_str());
+
+        /*
+         * Comparing the best firmware with the actual one, requesting an update if they differ.
+         */
+        if (best_firmware_info.software_version.major == node_info.software_version.major &&
+            best_firmware_info.software_version.minor == node_info.software_version.minor &&
+            best_firmware_info.software_version.vcs_commit == node_info.software_version.vcs_commit)
+        {
+            std::cout << "Firmware is already up-to-date" << std::endl;
+            return false;
+        }
+
+        /*
+         * The current implementation of FirmwareUpdateTrigger imposes a limitation on the maximum length of
+         * the firmware file path: it must not exceed 40 characters. This is NOT a limitation of UAVCAN itself.
+         *
+         * The firmware file name format we're currently using may procude names that exceed the length limitation,
+         * therefore we need to work around that. We do so by means of computing a hash (CRC64 in this example, but
+         * it obviously could be any other hash), and using it as the name of symlink to the firmware file.
+         *
+         * Aside from complying with the library's limitation, use of shorter lengths allows to somewhat reduce bus
+         * traffic resulting from file read requests from the updatee, as every request carries the name of the file.
+         *
+         * TODO: Do not use symlink if file name length does not exceed the limit.
+         */
+        out_firmware_file_path = makeFirmwareFileSymlinkName(best_file_name.c_str(), best_file_name.length());
+
+        const int symlink_res = ::symlink(best_file_name.c_str(), out_firmware_file_path.c_str());
+        if (symlink_res < 0)
+        {
+            std::cout << "Could not create symlink: " << symlink_res << std::endl;
+            return false;
+        }
+
+        std::cout << "Firmware file symlink: " << out_firmware_file_path.c_str() << std::endl;
+        return true;
     }
 
     /**
@@ -158,20 +214,82 @@ class ExampleFirmwareVersionChecker final : public uavcan::IFirmwareVersionCheck
 
     /*
      * This function is specific for this example implementation.
+     * It computes the name of a symlink to the firmware file.
+     * Please see explanation above, where the function is called from.
+     * The implementation is made so that it can work even on a deeply embedded system.
+     */
+    static FirmwareFilePath makeFirmwareFileSymlinkName(const char* file_name, unsigned file_name_length)
+    {
+        uavcan::DataTypeSignatureCRC hash;
+        hash.add(reinterpret_cast<const std::uint8_t*>(file_name), file_name_length);
+        auto hash_val = hash.get();
+
+        static const char CHARSET[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+        FirmwareFilePath out;
+        while (hash_val > 0)
+        {
+            out.push_back(CHARSET[hash_val % sizeof(CHARSET)]);
+            hash_val /= sizeof(CHARSET);
+        }
+
+        out += ".bin";
+        return out;
+    }
+
+    /*
+     * This function is specific for this example implementation.
+     * It extracts the version information from firmware file name.
+     * The implementation is made so that it can work even on a deeply embedded system.
+     * Assumed format is:
+     *      <node-name>-<hw-major>.<hw-minor>-<sw-major>.<sw-minor>.<vcs-hash-hex>.uavcan.bin
+     */
+    static uavcan::protocol::GetNodeInfo::Response parseFirmwareFileName(const char* name)
+    {
+        // Must be static in order to avoid heap allocation
+        static const auto extract_uint8 = [](unsigned& pos, const char* name)
+        {
+            std::uint8_t res = 0;
+            pos++;
+            while (std::isdigit(name[pos]))
+            {
+                res = res * 10 + int(name[pos] - '0');
+                pos++;
+            }
+            return res;
+        };
+
+        uavcan::protocol::GetNodeInfo::Response res;
+        unsigned pos = 0;
+
+        while (name[pos] != '-')
+        {
+            res.name.push_back(name[pos]);
+            pos++;
+        }
+
+        res.hardware_version.major = extract_uint8(pos, name);
+        res.hardware_version.minor = extract_uint8(pos, name);
+        res.software_version.major = extract_uint8(pos, name);
+        res.software_version.minor = extract_uint8(pos, name);
+
+        pos++;
+        res.software_version.vcs_commit = ::strtoul(name + pos, nullptr, 16);
+        res.software_version.optional_field_flags = res.software_version.OPTIONAL_FIELD_FLAG_VCS_COMMIT;
+
+        return res;
+    }
+
+    /*
+     * This function is specific for this example implementation.
      * It returns the firmware files available for given node info struct.
      */
     static std::vector<std::string> findAvailableFirmwareFiles(const uavcan::protocol::GetNodeInfo::Response& info)
     {
         std::vector<std::string> glob_matches;
 
-        /*
-         * Firmware file name pattern:
-         *      <node-name>-<hw-major>.<hw-minor>-<sw-major>.<sw-minor>.<vcs-hash-hex>.uavcan.bin
-         */
-        const std::string glob_pattern =
-            std::string(info.name.c_str()) + "-" +
-            std::to_string(info.hardware_version.major) + "." + std::to_string(info.hardware_version.minor) +
-            "-*.uavcan.bin";
+        const std::string glob_pattern = std::string(info.name.c_str()) + "-" +
+                                         std::to_string(info.hardware_version.major) + "." +
+                                         std::to_string(info.hardware_version.minor) + "-*.uavcan.bin";
 
         auto result = ::glob_t();
 
@@ -213,6 +331,13 @@ int main(int argc, const char** argv)
      * Typically, a node that serves as a firmware updater will also implement a dynamic node ID allocator.
      * Refer to the tutorial "Dynamic node ID allocation" to learn how to add a dynamic node ID allocator
      * to your node using libuavcan.
+     *
+     * Also keep in mind that in most real-world applications, features dependent on blocking APIs
+     * (such as this firmware update feature) will have to be implemented in a secondary thread in order to not
+     * interfere with real-time processing of the primary thread. In the case of this firmware updater, the
+     * interference may be caused by relatively intensive processing and blocking calls to the file system API.
+     * Please refer to the dedicated tutorial to learn how to implement a multi-threaded node, then use that
+     * knowledge to make this example application multithreaded (consider this an excercise).
      */
     uavcan::Node<16384> node(getCanDriver(), getSystemClock());
     node.setNodeID(self_node_id);
@@ -272,10 +397,13 @@ int main(int argc, const char** argv)
         throw std::runtime_error("Failed to start the file server: " + std::to_string(file_server_res));
     }
 
+    std::cout << "Started successfully" << std::endl;
+
     /*
      * Running the node normally.
      * All of the work will be done in background.
      */
+    node.setModeOperational();
     while (true)
     {
         const int res = node.spin(uavcan::MonotonicDuration::getInfinite());
